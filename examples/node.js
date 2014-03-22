e8866c6c1d109b87985682818e36d8e35e6c9bab
patr0nus <patronum@outlook.com>
2014-03-22 21:12:58 +0800

    Add sortedids

--- examples/node.js
+++ examples/node.js
@@ -5,10 +5,12 @@ var node = new Node(4321);
 node.connect([{ host: 'localhost', port: 6785 }]);
 
 node.on('online', function () {
+  console.log('Online ids:', node.sortedIds);
   console.log('I am online:', this.id);
 });
 
 node.on('node::online', function (newNode) {
+  console.log('Online ids:', node.sortedIds);
   console.log('Someone is online:', newNode.id);
   node.send(newNode.id, 'hello');
 });
@@ -18,5 +20,7 @@ node.on('node::hello', function (senderId) {
 });
 
 node.on('node::offline', function (offlineNodeId) {
+  
+  console.log('Online ids:', node.sortedIds);
   console.log('Someone is offline:', offlineNodeId);
 });
